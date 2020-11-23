9c878c49843f25608721b960ceb92d5cbb0c57d5
Sergey <stimval@gmail.com>
2020-11-23 20:25:17 +0400

    Fixed import in entity Person. Changed field types for storing time to LocalDateTime

--- src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
@@ -2,13 +2,13 @@ package ru.skillbox.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.skillbox.socialnetwork.model.Person;
 
 @SpringBootApplication
 public class SocialNetworkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SocialNetworkApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialNetworkApplication.class, args);
+  }
 
 }
--- src/main/java/ru/skillbox/socialnetwork/model/Person.java
+++ src/main/java/ru/skillbox/socialnetwork/model/Person.java
@@ -1,6 +1,6 @@
 package ru.skillbox.socialnetwork.model;
 
-import java.util.Calendar;
+import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,23 +19,23 @@ public class Person {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "first_name", nullable = false)
+  @Column(name = "first_name", nullable = false, length = 100)
   private String firstName;
 
-  @Column(name = "last_name", nullable = false)
+  @Column(name = "last_name", nullable = false, length = 100)
   private String lastName;
 
   @CreatedDate
-  @Column(name = "reg_date", nullable = false)
-  private Calendar regDate;
+  @Column(name = "reg_date", nullable = false, columnDefinition = "TIMESTAMP")
+  private LocalDateTime regDate;
 
-  @Column(name = "birth_date")
-  private Calendar birthDate;
+  @Column(name = "birth_date", columnDefinition = "TIMESTAMP")
+  private LocalDateTime birthDate;
 
   @Column(name = "e_mail", nullable = false)
   private String email;
 
-  @Column(name = "phone")
+  @Column(name = "phone", length = 12)
   private String phone;
 
   @Column(name = "password")
@@ -62,8 +62,8 @@ public class Person {
   @Column(name = "message_permission")
   private String messagePermission;
 
-  @Column(name = "last_online_time")
-  private Calendar lastOnlineTime;
+  @Column(name = "last_online_time", columnDefinition = "TIMESTAMP")
+  private LocalDateTime lastOnlineTime;
 
   @Column(name = "is_online")
   private int isOnline;
