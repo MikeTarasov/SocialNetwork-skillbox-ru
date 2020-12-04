1bd3f079e2478d0bdb493d5547fda0b26910c2c0
Sergey <stimval@gmail.com>
2020-12-04 16:34:08 +0400

    Method getCurrentUser()

--- src/main/java/ru/skillbox/socialnetwork/services/AccountService.java
+++ src/main/java/ru/skillbox/socialnetwork/services/AccountService.java
@@ -1,6 +1,9 @@
 package ru.skillbox.socialnetwork.services;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.skillbox.socialnetwork.model.entity.Person;
 import ru.skillbox.socialnetwork.repository.PersonRepository;
@@ -8,14 +11,25 @@ import ru.skillbox.socialnetwork.repository.PersonRepository;
 @Service
 public class AccountService {
 
-    private final PersonRepository personRepository;
+  private final PersonRepository personRepository;
 
-    @Autowired
-    public AccountService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+  @Autowired
+  public AccountService(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  public Person getCurrentUser() {
+
+    String email = SecurityContextHolder
+        .getContext()
+        .getAuthentication().getName();
+
+    Optional<Person> per = personRepository.findByEmail(email);
+
+    if (per.isEmpty()) {
+      throw new UsernameNotFoundException(" - User with : " + email + " not found");
     }
 
-public Person getCurrentUser() {
-        return null;
-    }
+    return per.get();
+  }
 }
